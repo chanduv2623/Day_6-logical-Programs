c5e89867ab34220be350491a2be4cbb06b4a1a7d
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-07-03 12:28:16 +0530

    Finding the given number as odd or even

--- DAy_6-Logical-Programs/Program.cs
+++ DAy_6-Logical-Programs/Program.cs
@@ -4,46 +4,14 @@ using System.Diagnostics.Metrics;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.Intrinsics.X86;
 using System;
+using System.ComponentModel.Design;
 
-namespace DAy_6_Logical_Programs
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int number;
-            Console.WriteLine("Enter a number:");
-            number = int.Parse(Console.ReadLine());
-
-            bool isPerfect = IsPerfectNumber(number);
-
-            if (isPerfect)
-            {
-                Console.WriteLine($"{number} is a perfect number.");
-            }
-            else
-            {
-                Console.WriteLine($"{number} is not a perfect number.");
-            }
-
-            Console.ReadLine();
-        }
-
-        static bool IsPerfectNumber(int number)
-        {
-            int sum = 0;
-
-            // Find the divisors and calculate the sum
-            for (int i = 1; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    sum += i;
-                }
-            }
-
-            // Check if the sum of divisors equals the number
-            return sum == number;
-        }
-    }
-}
+Console.WriteLine("Plese Enter Your Input Number");
+int n;
+n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+    Console.WriteLine(n + "Is an Even Number");
+else
+    Console.WriteLine(n + "Is an Odd Number");
+Console.ReadKey();
+           
