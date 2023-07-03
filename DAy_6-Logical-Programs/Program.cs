f438379c219c53e79952d436ada2a682b3295027
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-07-03 13:09:17 +0530

    Start/Stop Stopwatch

--- DAy_6-Logical-Programs/Program.cs
+++ DAy_6-Logical-Programs/Program.cs
@@ -6,41 +6,28 @@ using System.Runtime.Intrinsics.X86;
 using System;
 using System.ComponentModel.Design;
 using System.Diagnostics;
+using System.Dynamic;
 
 public class ReverseExample
 {
     public static void Main(string[] args)
     {
-        int numCoupons;
-        Console.WriteLine("Enter the number of coupon numbers to generate:");
-        numCoupons = int.Parse(Console.ReadLine());
+        Console.WriteLine("Stopwatch Simulation");
+        Console.WriteLine("Press ENTER to start the stopwatch.");
+        Console.ReadLine();
 
-        GenerateCouponNumbers(numCoupons);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        Console.WriteLine("Stopwatch is running.");
+        Console.WriteLine("Press ENTER to stop the stopwatch.");
+        Console.ReadLine();
+
+        stopwatch.Stop();
+
+        TimeSpan elapsed = stopwatch.Elapsed;
+        Console.WriteLine($"Elapsed Time: {elapsed.TotalSeconds} seconds");
 
         Console.ReadLine();
-    }
-
-    static void GenerateCouponNumbers(int numCoupons)
-    {
-        Random random = new Random();
-
-        for (int i = 1; i <= numCoupons; i++)
-        {
-            string couponNumber = GetCouponNumber(random, 10);
-            Console.WriteLine($"Coupon {i}: {couponNumber}");
-        }
-    }
-
-    static string GetCouponNumber(Random random, int length)
-    {
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        char[] couponChars = new char[length];
-
-        for (int i = 0; i < length; i++)
-        {
-            couponChars[i] = chars[random.Next(chars.Length)];
-        }
-
-        return new string(couponChars);
     }
 }
