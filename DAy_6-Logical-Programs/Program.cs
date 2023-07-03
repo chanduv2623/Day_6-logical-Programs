9fdb3ce64c9e772bb9351b5aff83419d1d186e66
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-07-03 13:19:07 +0530

    Fewest notes to be returned

--- DAy_6-Logical-Programs/Program.cs
+++ DAy_6-Logical-Programs/Program.cs
@@ -7,27 +7,35 @@ using System;
 using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Dynamic;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
 
 public class ReverseExample
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Stopwatch Simulation");
-        Console.WriteLine("Press ENTER to start the stopwatch.");
-        Console.ReadLine();
+        int[] availableNotes = { 2000, 500, 100, 50, 20, 10, 5, 1 };
 
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
+        Console.WriteLine("Enter the amount:");
+        int amount = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Stopwatch is running.");
-        Console.WriteLine("Press ENTER to stop the stopwatch.");
-        Console.ReadLine();
+        Console.WriteLine("Fewest notes to be returned:");
 
-        stopwatch.Stop();
+        for (int i = 0; i < availableNotes.Length; i++)
+        {
+            int notesCount = amount / availableNotes[i];
+            amount %= availableNotes[i];
 
-        TimeSpan elapsed = stopwatch.Elapsed;
-        Console.WriteLine($"Elapsed Time: {elapsed.TotalSeconds} seconds");
-
+            if (notesCount > 0)
+            {
+                Console.WriteLine($"{availableNotes[i]} - {notesCount} note(s)");
+            }
+        }
         Console.ReadLine();
     }
 }
+
+
+
+
+
