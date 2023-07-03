9f740de81abbace14f4820210f8c15e9daec07f7
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-07-03 13:00:06 +0530

    Generating coupon numbers

--- DAy_6-Logical-Programs/Program.cs
+++ DAy_6-Logical-Programs/Program.cs
@@ -5,19 +5,42 @@ using System.Runtime.ConstrainedExecution;
 using System.Runtime.Intrinsics.X86;
 using System;
 using System.ComponentModel.Design;
+using System.Diagnostics;
+
 public class ReverseExample
 {
     public static void Main(string[] args)
     {
-        int n, reverse = 0, rem;
-Console.Write("Enter a number: ");
-n = int.Parse(Console.ReadLine());
-while (n != 0)
-{
-    rem = n % 10;
-    reverse = reverse * 10 + rem;
-    n /= 10;
+        int numCoupons;
+        Console.WriteLine("Enter the number of coupon numbers to generate:");
+        numCoupons = int.Parse(Console.ReadLine());
+
+        GenerateCouponNumbers(numCoupons);
+
+        Console.ReadLine();
+    }
+
+    static void GenerateCouponNumbers(int numCoupons)
+    {
+        Random random = new Random();
+
+        for (int i = 1; i <= numCoupons; i++)
+        {
+            string couponNumber = GetCouponNumber(random, 10);
+            Console.WriteLine($"Coupon {i}: {couponNumber}");
+        }
+    }
+
+    static string GetCouponNumber(Random random, int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        char[] couponChars = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            couponChars[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(couponChars);
+    }
 }
-Console.Write("Reversed Number: " + reverse);       
-    }  
-  }  
